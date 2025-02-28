26f99002e6a03b59916efd804a61a730b14e29d2
Mario <mario@mariogl.com>
2025-02-28 18:11:56 +0100

    Add Deliveries Page

---
+++ src/delivery/pages/DeliveriesPage/DeliveriesPage.tsx
@@ -0,0 +1,5 @@
+const DeliveriesPage: React.FC = () => {
+  return <h2>Entregas</h2>;
+};
+
+export default DeliveriesPage;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router";
 import App from "../components/App/App";
+import DeliveriesPage from "../delivery/pages/DeliveriesPage/DeliveriesPage";
 
 const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
-        {/* <Route index element={<Navigate to="/deliveries" />} /> */}
+        <Route index element={<Navigate to="/deliveries" />} />
+        <Route path="deliveries" element={<DeliveriesPage />} />
       </Route>
     </Routes>
   );
